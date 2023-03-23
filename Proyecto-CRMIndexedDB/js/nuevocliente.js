1290af8af25d040eef03bf98f7179ecaa05f8500
Demian <demian.sotomayor.ur@gmail.com>
2023-03-23 14:11:31 -0300

    update errores

--- Proyecto-CRMIndexedDB/js/nuevocliente.js
+++ Proyecto-CRMIndexedDB/js/nuevocliente.js
@@ -9,20 +9,6 @@
         formulario.addEventListener('submit', validarCliente);
     });
 
-/* _______________________ Conectar con la base de datos _______________________ */
-
-    function conectarDB() {
-        const abrirConexion = window.indexedDB.open('crm', 1);
-
-        abrirConexion.onerror = function() {
-            console.log('Hubo un error')
-        };
-
-        abrirConexion.onsuccess = function() {
-            DB = abrirConexion.result;
-        };
-    }
-
     function validarCliente(e) {
         e.preventDefault();
 
@@ -32,7 +18,7 @@
         const telefono = document.querySelector('#telefono').value;
         const empresa = document.querySelector('#empresa').value;
 
-        if( nombre === '' || email === '' ||telefono === '' || empresa === '' ) {
+        if( nombre === '' || email === '' || telefono === '' || empresa === '' ) {
             imprimirAlerta('Todos los campos son obligatorios', 'error');
             
             return;
@@ -54,7 +40,6 @@
 
     function crearNuevoCliente(cliente) {
         const transaction = DB.transaction(['crm'], 'readwrite');
-        
         const objectStore = transaction.objectStore('crm');
 
         objectStore.add(cliente);
@@ -64,44 +49,12 @@
         };
 
         transaction.oncomplete = function() {
-            console.log('Cliente agregado');
-
             imprimirAlerta('El cliente se agregó correctamente');
 
             setTimeout(() => {
                 window.location.href = 'index.html';
             }, 3000);
         };
-    }
-
-/* _______________________ Imprimir mensaje al validar formulario _______________________ */
-
-    function imprimirAlerta(mensaje, tipo) {
-
-        const alerta = document.querySelector('.alerta');
-        
-        // !alerta Previene duplicado de alertas
-        if( !alerta ) { 
-
-            // Crear alerta
-            const divMensaje = document.createElement('div');
-            divMensaje.classList.add('px-4', 'py-3', 'rounded', 'max-w-lg', 'mx-auto', 'mt-6', 'text-center', 'border', 'alerta');
-
-            if( tipo === 'error' ) {
-                divMensaje.classList.add('bg-red-100', 'border-red-400', 'text-red-700');
-            } else {
-                divMensaje.classList.add('bg-green-100', 'border-green-400', 'text-green-700');
-            }
-
-            divMensaje.textContent = mensaje;
-
-            formulario.appendChild(divMensaje);
-
-            setTimeout(() => {
-                divMensaje.remove();
-            }, 3000);
-            
-        }
     }
 
 })();
